71804fc45856d016e3367de91a723cfd49f2c505
Joshua Douce <joshuadouce@gmail.com>
2019-07-05 13:55:42 +0100

    Observer Pattern

---
+++ ObserverPattern/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gClooney = new GClooney("I love my new wife");
+            var tSwift = new TSwift("1981 is now my favorite number.");
+
+            var firstFan = new Fan();
+            var secondFan = new Fan();
+
+            gClooney.AddFollower(firstFan);
+            gClooney.AddFollower(secondFan);
+            tSwift.AddFollower(secondFan);
+
+            gClooney.Tweet = "My wife didn't force me to tweet.";
+            tSwift.Tweet = "I love my new music.";
+
+            gClooney.RemoveFollower(secondFan);
+
+            gClooney.Tweet = "I am Batman";
+            Console.Read();
+        }
+    }
+}
